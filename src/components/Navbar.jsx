d2c3498d3561695d91353953f9b400be1fc4ede4
Micah <michkulien@gmail.com>
2024-06-15 13:05:06 +0100

    correct menu display for small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import { FaReact } from 'react-icons/fa'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineClose } from 'react-icons/md'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false)
+
+  function showMenuHandler() {
+    setShowMenu((prev) => {
+      return !prev
+    })
+  }
+
   return (
     <div className='navbar'>
       <div className='wrapper-navbar'>
@@ -10,7 +21,10 @@ function Navbar() {
             <FaReact className='react-logo' /> React Jobs
           </NavLink>
         </div>
-        <div className='menu'>
+        <div className='menu-icon' onClick={showMenuHandler}>
+          {showMenu ? <MdOutlineClose /> : <GiHamburgerMenu />}
+        </div>
+        <div className={showMenu ? 'menu show-menu' : 'menu hide-menu'}>
           <ul>
             <li>
               <NavLink
